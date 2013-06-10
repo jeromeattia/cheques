868f156f3387ad83a8e23d305c09d34f11910eab
jerome <doc.jerome.attia@gmail.com>
2013-06-10 12:07:24 +0200

    intégration correcte des décimales

--- lib/cheque_writer.rb
+++ lib/cheque_writer.rb
@@ -1,5 +1,28 @@
 require 'numbers_and_words'
+require 'prawn'
 
 class ChequeWriter
 
+
+  def convert(number)
+    str =  I18n.with_locale(:fr) { number.floor.to_words }
+    str = str + ' Euros '  +  (((number - number.to_int).round 2)*100).to_int.to_s + ' cents '
+
+  end
+
+  def pad_with_stars(string, line_width)
+     string.ljust(line_width,'*')
+  end
+
+  def generate_pdf(str)
+    pdf = Prawn::Document.new
+    pdf.text str
+    pdf.render_file "cheque.pdf"
+  end
 end
+
+conv = ChequeWriter.new
+
+puts str = conv.convert(134.60)
+puts str = conv.pad_with_stars(str, 50)
+conv.generate_pdf(str)
---
+++ test/cheque_writer_test.rb
@@ -0,0 +1,18 @@
+require 'test/unit'
+require_relative  '../lib/cheque_writer'
+
+class ChequeWriterTest < Test::Unit::TestCase
+
+  def setup
+
+  end
+
+  def teardown
+
+  end
+
+
+  def test_fail
+    fail('Not implemented')
+  end
+end
